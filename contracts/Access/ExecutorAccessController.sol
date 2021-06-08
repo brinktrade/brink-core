96efc0c0250bca518197782b15d26cfd84d9c303
Kyle <the.big.kb123@gmail.com>
2021-06-08 14:50:11 -0400

    Slight modifications to the contract, and unit tests for the contract

--- contracts/Access/ExecutorAccessController.sol
+++ contracts/Access/ExecutorAccessController.sol
@@ -20,32 +20,25 @@ contract ExecutorAccessController {
   /// Constructor, which stores the address of the owner
   /// @param _owner the specified owner address
   constructor(address _owner) {
-    require(_owner != address(0), "ExecutorAccess: Owner cannot be the null address");
     owner = _owner;
   }
 
   /// addAdmin, which allows owner to add an Admin
   /// @param _admin the specified Admin Addresss to add
   function addAdmin(address _admin) external onlyOwner {
-    require(_admin != address(0), "ExecutorAccess: Admin cannot be the null address");
-    require(_isAdmin(_admin) == false, "ExecutorAccess: Address given is already an admin");
     isAdminMap[_admin] = true;
   }
 
   /// removeAdmin, which allows owner to remove an Admin
   /// @param _admin the specified Admin Addresss to remove
   function removeAdmin(address _admin) external onlyOwner {
-    require(_admin != address(0), "ExecutorAccess: Admin cannot be the null address");
-    require(_isAdmin(_admin), "ExecutorAccess: Address given is not an admin");
     isAdminMap[_admin] = false;
   }
 
   /// addExecutor, which allows an Admin to add an Executor
   /// @param _executor the specified Executor Addresss to add
   function addExecutor(address _executor) external onlyAdmin {
-    require(_executor != address(0), "ExecutorAccess: Executor cannot be the null address");
-    require(_isExecutor(_executor) == false, "ExecutorAccess: Address given is already an executor");
-    require(executorAdminMap[_executor] == address(0), "ExecutorAccess: Address given already has an admin associated with it");
+    require(executorAdminMap[_executor] == address(0), "ExecutorAccessController: Address given already has an admin associated with it");
     executorAdminMap[_executor] = msg.sender;
     isExecutorMap[_executor] = true;
   }
@@ -53,7 +46,7 @@ contract ExecutorAccessController {
   /// removeExecutor, which allows an Admin to remove an Executor they added or Owner to remove any executor
   /// @param _executor the specified Executor Addresss to remove
   function removeExecutor(address _executor) external {
-    require(_isExecutorOwner(_executor), "ExecutorAccess: Admin cannot remove an executor they did not add.");
+    require(_isExecutorOwner(_executor), "ExecutorAccessController: Address cannot remove an executor they did not add");
     _removeExecutor(_executor);
   }
 
@@ -86,10 +79,15 @@ contract ExecutorAccessController {
     return isAdminMap[_admin];
   }
 
+  /// isOwner, returns true if specified address is the Owner Address
+  /// @param _owner the specified Owner Address
+  /// @return if the address is the Owner Address
+  function isOwner(address _owner) external view returns(bool) {
+    return owner == _owner;
+  }
+
   /// @dev internal helper that does the work of removing an Executor Address with checks
   function _removeExecutor(address _executor) internal {
-    require(_executor != address(0), "ExecutorAccess: Executor cannot be the null address");
-    require(_isExecutor(_executor), "ExecutorAccess: Address given is not an executor");
     executorAdminMap[_executor] = address(0);
     isExecutorMap[_executor] = false;
   }
@@ -98,7 +96,7 @@ contract ExecutorAccessController {
   modifier onlyOwner {
     require(
       msg.sender == owner,
-      "ExecutorAccess: Only the owner can call this function"
+      "ExecutorAccessController: Only the owner can call this function"
     );
     _;
   }
@@ -107,7 +105,7 @@ contract ExecutorAccessController {
   modifier onlyAdmin {
     require(
       isAdminMap[msg.sender],
-      "ExecutorAccess: Only an admin can call this function"
+      "ExecutorAccessController: Only an admin can call this function"
     );
     _;
   }
---
+++ test/ExecutorAccessController.test.js
@@ -0,0 +1,115 @@
+const { ethers } = require('hardhat')
+const { chaiSolidity } = require('@brinkninja/test-helpers')
+const { expect } = chaiSolidity()
+
+describe.only("ExecutorAccessController", function() {
+  beforeEach(async function () {
+    const [ ownerAccount, adminAccount, executorAccount , adminAccount2, executorAccount2, randomAccount] = await ethers.getSigners()
+    this.ownerAccount = ownerAccount
+    this.adminAccount = adminAccount
+    this.executorAccount = executorAccount
+    this.adminAccount2 = adminAccount2
+    this.executorAccount2 = executorAccount2
+    this.randomAccount = randomAccount
+    const ExecutorAccessController = await ethers.getContractFactory("ExecutorAccessController");
+    const executorAccessController = await ExecutorAccessController.deploy(this.ownerAccount.address)
+    this.executorAccessController = executorAccessController
+  })
+
+  describe("Owner Actions", function() {
+    it("Deploys the contract with specified owner address as owner", async function() {
+      expect(await this.executorAccessController.isOwner(this.ownerAccount.address)).to.equal(true);
+    });
+
+    it("Allows owner to add an admin address", async function() {
+      await this.executorAccessController.addAdmin(this.adminAccount.address)
+      expect(await this.executorAccessController.isAdmin(this.adminAccount.address)).to.equal(true);
+    });
+
+    it("Allows owner to remove an admin address", async function() {
+      await this.executorAccessController.addAdmin(this.adminAccount.address)
+      expect(await this.executorAccessController.isAdmin(this.adminAccount.address)).to.equal(true);
+      await this.executorAccessController.removeAdmin(this.adminAccount.address)
+      expect(await this.executorAccessController.isAdmin(this.adminAccount.address)).to.equal(false);
+    });
+
+    it("Allows owner to remove an executor address", async function() {
+      await this.executorAccessController.addAdmin(this.adminAccount.address)
+      await this.executorAccessController.connect(this.adminAccount).addExecutor(this.executorAccount.address)
+      expect(await this.executorAccessController.isExecutor(this.executorAccount.address)).to.equal(true);
+      await this.executorAccessController.connect(this.ownerAccount).removeExecutor(this.executorAccount.address)
+      expect(await this.executorAccessController.isExecutor(this.executorAccount.address)).to.equal(false);
+    });
+
+    it("Prevents owner from adding an executor address, reverts with: 'ExecutorAccessController: Only an admin can call this function'", async function() {
+      await expect(this.executorAccessController.addExecutor(this.executorAccount.address))
+        .to.be.revertedWith('ExecutorAccessController: Only an admin can call this function')
+    });
+  });
+
+  describe("Admin Actions", function() {
+    beforeEach(async function () {
+      await this.executorAccessController.addAdmin(this.adminAccount.address)
+      await this.executorAccessController.addAdmin(this.adminAccount2.address)
+    })
+    
+    it("Allows admin to add an owned executor address", async function() {
+      await this.executorAccessController.connect(this.adminAccount).addExecutor(this.executorAccount.address)
+      expect(await this.executorAccessController.isExecutor(this.executorAccount.address)).to.equal(true);
+    });
+
+    it("Allows admin to remove an owned executor address", async function() {
+      await this.executorAccessController.connect(this.adminAccount).addExecutor(this.executorAccount.address)
+      expect(await this.executorAccessController.isExecutor(this.executorAccount.address)).to.equal(true);
+      await this.executorAccessController.connect(this.adminAccount).removeExecutor(this.executorAccount.address)
+      expect(await this.executorAccessController.isExecutor(this.executorAccount.address)).to.equal(false);
+    });
+
+    it("Prevents admin from adding an admin, reverts with 'ExecutorAccessController: Only the owner can call this function'", async function() {
+      await expect(this.executorAccessController.connect(this.adminAccount).addAdmin(this.adminAccount2.address))
+        .to.be.revertedWith('ExecutorAccessController: Only the owner can call this function')
+    });
+
+    it("Prevents admin1 from removing an admin2 owned executor address, reverts with 'ExecutorAccessController: Admin cannot remove an executor they did not add'", async function() {
+      await this.executorAccessController.connect(this.adminAccount2).addExecutor(this.executorAccount2.address)
+      expect(await this.executorAccessController.isExecutor(this.executorAccount2.address)).to.equal(true);
+      await expect(this.executorAccessController.connect(this.adminAccount).removeExecutor(this.executorAccount2.address))
+        .to.be.revertedWith('ExecutorAccessController: Address cannot remove an executor they did not add')
+    });
+
+    it("Prevents admin1 from adding an admin2 owned executor address, reverts with 'ExecutorAccessController: Address given already has an admin associated with it'", async function() {
+      await this.executorAccessController.connect(this.adminAccount2).addExecutor(this.executorAccount2.address)
+      expect(await this.executorAccessController.isExecutor(this.executorAccount2.address)).to.equal(true);
+      await expect(this.executorAccessController.connect(this.adminAccount).addExecutor(this.executorAccount2.address))
+        .to.be.revertedWith('ExecutorAccessController: Address given already has an admin associated with it')
+    });
+  });
+
+  describe("Random Account Actions", function() {
+    beforeEach(async function () {
+      await this.executorAccessController.addAdmin(this.adminAccount.address)
+      await this.executorAccessController.connect(this.adminAccount).addExecutor(this.executorAccount.address)
+    })
+
+    it("Prevents random account from adding an admin address, reverts with 'ExecutorAccessController: Only the owner can call this function'", async function() {
+      await expect(this.executorAccessController.connect(this.randomAccount).addAdmin(this.adminAccount2.address))
+        .to.be.revertedWith('ExecutorAccessController: Only the owner can call this function')
+    });
+
+    it("Prevents random account from adding an executor address, reverts with 'ExecutorAccessController: Only an admin can call this function'", async function() {
+      await expect(this.executorAccessController.connect(this.randomAccount).addExecutor(this.executorAccount2.address))
+        .to.be.revertedWith('ExecutorAccessController: Only an admin can call this function')
+    });
+
+    it("Prevents random account from removing an admin address, reverts with 'ExecutorAccessController: Only the owner can call this function'", async function() {
+      await expect(this.executorAccessController.connect(this.randomAccount).removeAdmin(this.adminAccount.address))
+        .to.be.revertedWith('ExecutorAccessController: Only the owner can call this function')
+    });
+
+    it("Prevents random account from removing an executor address, reverts with 'ExecutorAccessController: Address cannot remove an executor they did not add'", async function() {
+      await expect(this.executorAccessController.connect(this.randomAccount).removeExecutor(this.executorAccount.address))
+        .to.be.revertedWith('ExecutorAccessController: Address cannot remove an executor they did not add')
+    });
+
+  });
+});
