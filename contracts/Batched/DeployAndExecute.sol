450f6d5c21fe444ca4e348e9fcfe44afbecd61c6
Kyle Bryant <the.big.kb123@gmail.com>
2021-09-15 11:08:11 -0400

    Audit Issue 3: Specify singletonFactory as internal

--- contracts/Batched/DeployAndExecute.sol
+++ contracts/Batched/DeployAndExecute.sol
@@ -7,7 +7,7 @@ import "../Interfaces/ISingletonFactory.sol";
  * @dev Contract for batching CREATE2 contract deployment and an initial call into 1 tx
  */
 contract DeployAndExecute {
-  ISingletonFactory immutable singletonFactory;
+  ISingletonFactory internal immutable singletonFactory;
 
   /**
   * @dev The constructor sets a SingletonFactory
