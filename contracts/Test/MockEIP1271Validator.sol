4ad07e3b047415e6686c3035369e82db07bc1a26
Kyle <the.big.kb123@gmail.com>
2021-09-15 14:49:55 -0400

    Audit Issue 1: Change to carrots

--- contracts/Test/MockEIP1271Validator.sol
+++ contracts/Test/MockEIP1271Validator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.7.6;
+pragma solidity ^0.7.6;
 
 contract ISignatureValidatorConstants {
   // bytes4(keccak256("isValidSignature(bytes,bytes)")
