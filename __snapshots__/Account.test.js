d5d1127d5656b21aef321db1095a2dfa93b94cf3
Mike Calvanese <mfcalvanese@gmail.com>
2021-11-17 15:38:12 -0500

    gas snapshot for custom error change

--- __snapshots__/Account.test.js
+++ __snapshots__/Account.test.js
@@ -4,4 +4,4 @@ exports['Account delegateCall() gas cost 1'] = 32262
 
 exports['Account metaDelegateCall() gas cost 1'] = 39464
 
-exports['Account metaDelegateCall_EIP1271() gas cost 1'] = 52144
+exports['Account metaDelegateCall_EIP1271() gas cost 1'] = 52147
