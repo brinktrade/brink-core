07b9d79267a352d706d250a1f8fce928c1b02c0d
Mike Calvanese <mfcalvanese@gmail.com>
2021-11-16 21:30:44 -0500

    upgrade to solidity 0.8.10 with abicoder v1

--- contracts/Proxy/ProxySettable.sol
+++ contracts/Proxy/ProxySettable.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.7.6;
+pragma solidity =0.8.10;
+pragma abicoder v1;
 
 import "./ProxyStorage.sol";
 
--- contracts/Test/MockEIP1271Validator.sol
+++ contracts/Test/MockEIP1271Validator.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.7.6;
+pragma solidity =0.8.10;
+pragma abicoder v1;
 
 contract ISignatureValidatorConstants {
   // bytes4(keccak256("isValidSignature(bytes,bytes)")
