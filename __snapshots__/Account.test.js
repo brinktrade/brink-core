8f98037fbf2949cf73598a1ccfccd996e9d1e35a
Mike Calvanese <mfcalvanese@gmail.com>
2021-11-17 12:35:07 -0500

    gas snapshots after ECDSA.sol change to older version

--- __snapshots__/Account.test.js
+++ __snapshots__/Account.test.js
@@ -2,6 +2,6 @@ exports['Account externalCall() gas cost 1'] = 32378
 
 exports['Account delegateCall() gas cost 1'] = 32262
 
-exports['Account metaDelegateCall() gas cost 1'] = 39658
+exports['Account metaDelegateCall() gas cost 1'] = 39464
 
 exports['Account metaDelegateCall_EIP1271() gas cost 1'] = 52144
