5014986ddea21645abb7f6b4b19a255eb9ff5558
Mike Calvanese <mfcalvanese@gmail.com>
2021-11-17 17:35:29 -0500

    make Account.sol external functions `payable`
    it's more gas efficient, and there may be certain use cases where they should accept ether value

--- __snapshots__/Account.test.js
+++ __snapshots__/Account.test.js
@@ -1,7 +1,7 @@
-exports['Account externalCall() gas cost 1'] = 32421
+exports['Account externalCall() gas cost 1'] = 32397
 
-exports['Account delegateCall() gas cost 1'] = 32317
+exports['Account delegateCall() gas cost 1'] = 32293
 
-exports['Account metaDelegateCall() gas cost 1'] = 38996
+exports['Account metaDelegateCall() gas cost 1'] = 38972
 
-exports['Account metaDelegateCall_EIP1271() gas cost 1'] = 51095
+exports['Account metaDelegateCall_EIP1271() gas cost 1'] = 51071
