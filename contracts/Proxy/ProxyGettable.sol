db8847b1f7ba89b2ac43a405aaa59cf7fce1c836
Mike Calvanese <mfcalvanese@gmail.com>
2021-11-22 16:15:21 -0500

    update comment in ProxyGettable

--- contracts/Proxy/ProxyGettable.sol
+++ contracts/Proxy/ProxyGettable.sol
@@ -3,12 +3,13 @@ pragma solidity =0.8.10;
 pragma abicoder v1;
 
 /**
- * @dev Exposes getter functions for Proxy.sol and contracts that are intended to receive
+ * @dev Exposes proxyOwner() view function for contracts that are intended to receive
  * delegatecall data from a Proxy instance.
  */
 abstract contract ProxyGettable {
   /**
    * @dev Returns the proxy owner address
+   * @notice When run by a Proxy.sol delegatecall, this function returns the value of the OWNER constant
    */
   function proxyOwner() public view returns (address _proxyOwner) {
     assembly {
