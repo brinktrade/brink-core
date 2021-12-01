03ccd9886318092e6898a3fab2ebd7928876b24b
Mike Calvanese <mfcalvanese@gmail.com>
2021-12-01 17:29:22 -0500

    update deployment salt

--- __snapshots__/DeployAndCall.test.js
+++ __snapshots__/DeployAndCall.test.js
@@ -1,1 +1,1 @@
-exports['DeployAndCall deployAndCall() with callData that does not revert gas cost 1'] = 94812
+exports['DeployAndCall deployAndCall() with callData that does not revert gas cost 1'] = 94800
